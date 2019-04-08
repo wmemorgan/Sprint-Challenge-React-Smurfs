2d01b0980fe5a4640e462c07f4766168257653c2
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-04-08 13:20:30 +0800

    Remove unused component

--- client/src/components/SmurfComponents/Smurfs.js
+++ client/src/components/SmurfComponents/Smurfs.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 import { SmurfListContainer, Preview } from './SmurfStyleComponents'
-import Smurf from './Smurf';
 
 const Smurfs = props => {
     const { smurfs } = props
